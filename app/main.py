d11a054cd373d8b58c1d73582a1870c82583ab7d
lasofiko <la.sofaa@yandex.ru>
2026-03-23 16:04:31 +0300

    update main

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.include_router(router, prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Адрес фронтенда
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
